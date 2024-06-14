45f8c4b1d856350d0458d2ccbd833200a1ede9a6
anon121213 <anon121213@mail.ru>
2024-06-14 13:42:18 +0300

    enemyForce

--- Assets/Scripts/Enemyes/HP/EnemyDamage.cs
+++ Assets/Scripts/Enemyes/HP/EnemyDamage.cs
@@ -9,17 +9,24 @@ public class EnemyDamage : MonoBehaviour, IDamageble
     [SerializeField] private float _pushForce;
     [SerializeField] private GameObject _xp;
     [SerializeField] private int Hp = 100;
+    [SerializeField] private float drag = 1.5f;
     
     [NonSerialized] public Transform _emptyXp;
     
     private GameObject _player;
     private WinSystem _winSystem;
+    private Rigidbody2D _rb;
     
     [Inject] private DiContainer _diContainer;
     [Inject] public void GetPlayer(GameObject player, WinSystem winSystem)
     {
         _player = player;
         _winSystem = winSystem;
+    }
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -49,10 +56,10 @@ public class EnemyDamage : MonoBehaviour, IDamageble
         if (_playerHp > 0)
         {
             var pushDirection = -(transform.position - _player.transform.position).normalized;
-            var _playerRb = _player.gameObject.GetComponent<Rigidbody2D>();
             
-            _playerRb.AddForce(pushDirection * _pushForce * 1000);
-            
+            _rb.AddForce(-pushDirection * _pushForce * 1000, ForceMode2D.Impulse);
+            _rb.drag = drag;
+
             var playerHp = _player.gameObject.GetComponent<PlayerStats>();
             playerHp.Hp -= 1;
         }
